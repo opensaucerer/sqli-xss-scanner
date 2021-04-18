3f51e3da62e946eca12df036583e9814021a9121
Samperfect <lovedayperfection1@gmail.com>
2021-04-18 17:29:04 +0100

    added a loader to the scan page

--- static/js/index.js
+++ static/js/index.js
@@ -1,1 +1,20 @@
+const body = document.querySelector('body');
+const section = document.querySelector('section');
 
+const loader = '<div id="loader_div" class="loader_div"></div>';
+
+const scan = document.querySelector('#submit');
+const form = document.querySelector('form');
+const input = document.querySelector('#url');
+
+form.onsubmit = async function () {
+  //   body.innerHTML = loader;
+  url = input.value;
+  console.log(url);
+  section.style.display = 'none';
+  body.innerHTML += loader;
+
+  location.href = `report?url=${url}`;
+
+  //   response = await fetch(`report?url=${url}`);
+};
