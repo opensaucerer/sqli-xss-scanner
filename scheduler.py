948c4a798b230531a2bcce8fbeec6da306d78c5d
Perfection Loveday <lovedayperfection1@gmail.com>
2021-08-27 00:42:46 +0100

    add scheduler for the pen testing app

--- scheduler.py
+++ scheduler.py
@@ -3,18 +3,50 @@ load_dotenv()
 import datetime
 import os
 import pymongo
+import requests
 
 client = pymongo.MongoClient(os.environ.get('MONGO_URI'))
 db = client.pentest
 
 
-def save_data_to_mongo(data):
-    response = db.test.insert_one(data)
+def get_all_scan():
+    response = db.schedule.find()
+    return response
+
+
+def check(data):
+    time = datetime.datetime.utcnow().time().hour
+    if data['freq'] == 'twice':
+        if time >= 24 and time < 2:
+            res = requests.get(
+                f'http://localhost:5000/w_report?id={data["_id"]}&url={data["url"]}&email={data["email"]}')
+
+        elif time >= 12 and time < 13:
+            res = requests.get(
+                f'http://localhost:5000/w_report?id={data["_id"]}&url={data["url"]}&email={data["email"]}')
+
+    else:
+        if time >= 12 and time < 13:
+            res = requests.get(
+                f'http://localhost:5000/w_report?id={data["_id"]}&url={data["url"]}&email={data["email"]}')
+
+
+def scan_d(data):
+    check(data)
+    return True
+
+
+def scan_w(data):
+    day = datetime.datetime.utcnow().date().weekday()
+    if day == 5:
+        check(data)
+    return True
 
 
 if __name__ == "__main__":
-    date_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    data = {}
-    data["text"] = "Hello World"
-    data["date_time"] = date_time
-    save_data_to_mongo(data)
+    scans = get_all_scan()
+    for scan in scans:
+        if scan['latency'] == 'daily':
+            scan_d(scan)
+        else:
+            scan_w(scan)
